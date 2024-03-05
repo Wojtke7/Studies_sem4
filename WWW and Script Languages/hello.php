1a52f34de04830501286ea926422f85a421ced6b
wmarcela <wmarcela@student.agh.edu.pl>
2024-03-05 10:33:50 +0100

    first touch with php

---
+++ WWW and Script Languages/hello.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "Hello World<br>\n";
+        $date = date('j-m-y, h:i:s');
+        $x = 5;
+        $y = 77;
+        $multiplication = $x * $y;
+        echo "Date is: {$date} lol <br>";
+        echo "Multiplication is: {$multiplication}";
+    ?>
+</body>
+</html>
