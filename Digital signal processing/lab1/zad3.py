fc8f2dd74e328fccc8a4a5f49a750bf7f5bb79d3
Wojtke7 <wojtekmarcela@interia.pl>
2024-03-06 10:10:33 +0100

    feat: changes in task 3

--- Digital signal processing/lab1/zad3.py
+++ Digital signal processing/lab1/zad3.py
@@ -1,76 +1,53 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import loadmat
-
-
-def mycorr(x, y):
-    # Obliczanie korelacji wzajemnej dwóch wektorów
-    # Korelację oblicza się jako sumę wartości obu sygnałów przemnożonych przez siebie
-    # x, y - wektory wejściowe
-    # r - wektor wynikowy korelacji wzajemnej
-    # lags - opóźnienia dla każdej wartości w r
-    n = len(x)
-    m = len(y)
-
-    # obliczenie średnich wartości obu wektorów
-    x_mean = np.mean(x)
-    y_mean = np.mean(y)
-
-    # obliczenie odchyleń standardowych obu wektorów
-    x_std = np.std(x)
-    y_std = np.std(y)
-
-    r = np.zeros(n + m - 1)
-    lags = np.arange(-n + 1, m)
-
-    # wyliczenie korelacji wzajemnej
-    for i in range(len(r)):
-        if lags[i] < 0:
-            r[i] = np.sum((x[0:n + lags[i]] - x_mean) * (y[-lags[i]:m] - y_mean))
-        elif lags[i] == 0:
-            r[i] = np.sum((x - x_mean) * (y - y_mean))
-        else:
-            r[i] = np.sum((x[lags[i]:n] - x_mean) * (y[0:m - lags[i]] - y_mean))
-
-        r[i] = r[i] / (x_std * y_std * (n - abs(lags[i])))
-
-    return r
-
+from corr_fun import corr_fun
 
 data = loadmat('adsl_x.mat')
-x = np.array(data['x'])
-prefix_len = 32  # długość prefiksu
-frame_len = 512  # długość ramki
-package_len = prefix_len + frame_len  # długość ramki i prefiksu
+signal = np.array(data['x'])
 
-max_corr = 0  # początkowa wartość maksymalnej korelacji
-st_prefix_probe = np.zeros((3, 1))
+prefix_length = 32
+frame_length = 512
+package_length = prefix_length + frame_length
 
-for i in range(len(x) // 3):  # pętla po całym sygnale
-    if (i + 3 * package_len) > len(x):
+max_correlation = 0
+start_prefix_positions = np.zeros((3, 1))  # wiemy że są 3 prefiksy
+
+for i in range(len(signal) // 3):
+    # Sprawdzenie czy nie wyszliśmy poza zakres sygnału
+    if (i + 3 * package_length) > len(signal):
         break
 
-    max_corr_grup = 0
-    tmp_prefix_probe = np.zeros((3, 1))
+    total_correlation = 0
+    tmp_start_prefix_positions = np.zeros((3, 1))
+
+    # Wewnętrzna pętla iteruje po trzech prefiksach. Dla każdego prefiksu obliczamy jego korelację
+    # z kopią kolejnego bloku danych (512 próbek). Sumujemy te korelacje dla każdego prefiksu w zmiennej
+    # total_correlation.
 
     for j in range(3):
-        # według instrukcji pakiety występują idealnie po sobie dlatego:
-        # trzeba znaleść takie miejsca dla których suma 3 korelacji będzie największa
-        prefix = x[i + j * package_len: i + j * package_len + prefix_len]  # prefiks
-        tmp_prefix_probe[j, 0] = i + j * package_len
+        prefix = signal[i + j * package_length: i + j * package_length + prefix_length]
 
-        copy_probe_block = x[i + j * package_len + frame_len: i + j * package_len + frame_len + prefix_len]
-        # korelacja między pierwszymi 32 próbkami a ostatnimi 32 próbkami w oknie
-        corr = mycorr(prefix, copy_probe_block)  # Moja funkcja korelacji
+        tmp_start_prefix_positions[j, 0] = i + j * package_length
 
-        max_corr_grup += np.mean(corr)
+        # Zmienna do przechowywania kolejnego bloku danych
+        next_frame_prefix_copy = signal[i + j * package_length + frame_length:
+                                  i + j * package_length + frame_length + prefix_length]
 
-    if max_corr_grup > max_corr:  # jeśli korelacja jest większa niż dotychczasowa maksymalna
-        max_corr = max_corr_grup  # aktualizacja maksymalnej korelacji
-        st_prefix_probe = tmp_prefix_probe  # początek prefiksu
+        # Obliczenie korelacji
+        corr = corr_fun(prefix, next_frame_prefix_copy)
+        total_correlation += np.mean(corr)
 
-plt.plot(x)
+    if total_correlation > max_correlation:
+        max_correlation = total_correlation
+        start_prefix_positions = tmp_start_prefix_positions
+
+plt.figure(figsize=(10, 6))
+plt.plot(signal, "r-", label='Signal')
 for i in range(3):
-    plt.plot(st_prefix_probe[i, 0], 0, 'rx')
-plt.legend(['signal'])
+    plt.plot(start_prefix_positions[i, 0], 0, 'bo', label='Prefix beginning' if i == 0 else "")
+plt.xlabel('Probe index')
+plt.ylabel('Amplitude')
+plt.legend()
+plt.grid(True)
 plt.show()
