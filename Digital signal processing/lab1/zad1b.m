25fec8553879f5006762ab779472918ea75f751a
Wojtke7 <wojtekmarcela@interia.pl>
2024-03-06 09:37:42 +0100

    feat: lab01 in matlab

--- Digital signal processing/lab1/zad1b.m
+++ Digital signal processing/lab1/zad1b.m
@@ -3,17 +3,13 @@ f = @(t) sin(50 *2*pi*t);
 close all;
 hold on;
 
-autoplot_const_func(f, 1, ...
-    containers.Map({'b-', 'g-o', 'r -o', 'k-o'}, ...
-                   {10^4,   51,    50,    49}));
+autoplot_fun(f, 1, containers.Map({'b-', 'g-o', 'r -o', 'k-o'},{10^4,   51,    50,    49}));
 title('1.B1');
 pause;
 close all;
 hold on;
 
-autoplot_const_func(f, 1, ...
-    containers.Map({'b-', 'g-o', 'r-o', 'k-o'}, ...
-                   {10^4,   26,    25,    24}));
+autoplot_fun(f, 1,containers.Map({'b-', 'g-o', 'r-o', 'k-o'}, {10^4,   26,    25,    24}));
 
 title('1.B2');
 pause;
